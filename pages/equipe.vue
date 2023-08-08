f61cb8d596d3c75791995d976c19aea2c6bd177e
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2023-08-08 09:38:08 -0400

    Updated the team page

--- pages/equipe.vue
+++ pages/equipe.vue
@@ -15,8 +15,8 @@
 					</div>
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1">Mgr. Jean-Pierre Joly</h4>
-							<p class="mb-0">Prêtre collaborateur</p>
+							<h4 class="mb-1">Charles-Arthur Fontaine</h4>
+							<p class="mb-0">Agent d'évangélisation</p>
 						</div>
 					</div>
 				</div>
@@ -24,14 +24,12 @@
 				<div class="row gx-md-8 gx-xl-12 align-items-center">
 					<div class="col-lg-6 mt-4">
 						<div>
-							<h4 class="mb-1">L'abbé Pierre Rivard</h4>
-							<p class="mb-0">Prêtre collaborateur</p>
+							<h4 class="mb-1 mt-4">Stéphanie Fournier</h4>
+							<p class="mb-0">Agente d'évangélisation</p>
 						</div>
 					</div>
 					<div class="col-lg-6 mt-4">
 						<div>
-							<h4 class="mb-1">M. Charles-Arthur Fontaine</h4>
-							<p class="mb-0">Agent d'évangélisation</p>
 						</div>
 					</div>
 				</div>
@@ -39,8 +37,6 @@
 				<div class="row gx-md-8 gx-xl-12 align-items-center">
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1 mt-4">Mme. Stephanie Fournier</h4>
-							<p class="mb-0">Agente d'évangélisation</p>
 						</div>
 					</div>
 				</div>
@@ -50,14 +46,27 @@
 				<div class="row gx-md-8 gx-xl-12 align-items-center">
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1">Mme. Francine Leduc</h4>
-							<p class="mb-0">Adjointe administrative</p>
+							<h4 class="mb-1">Manon Desjardins</h4>
+							<p class="mb-0">Commis-Comptable</p>
 						</div>
 					</div>
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1">Mme. Manon Desjardins</h4>
-							<p class="mb-0">Comptable</p>
+						</div>
+					</div>
+				</div>
+
+				<h3 class="display-4 mt-7  mb-5">Soutien</h3>
+
+				<div class="row gx-md-8 gx-xl-12 align-items-center">
+					<div class="col-lg-6">
+						<div>
+							<h4 class="mb-1">Charles Nantel</h4>
+							<p class="mb-0">Sacristain</p>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div>
 						</div>
 					</div>
 				</div>
@@ -67,13 +76,28 @@
 				<div class="row gx-md-8 gx-xl-12 align-items-center">
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1">M. René Bourgeault</h4>
+							<h4 class="mb-1">René Bourgeault</h4>
 							<p class="mb-0">Président</p>
 						</div>
 					</div>
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1">M. Pierre Potvin</h4>
+							<h4 class="mb-1">Martin Tremblay</h4>
+							<p class="mb-0">Vice-président</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row gx-md-8 gx-xl-12 align-items-center">
+					<div class="col-lg-6">
+						<div>
+							<h4 class="mb-1 mt-4">Pierre Potvin</h4>
+							<p class="mb-0">Marguillier</p>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div>
+							<h4 class="mb-1 mt-4">Marc Rompré</h4>
 							<p class="mb-0">Marguillier</p>
 						</div>
 					</div>
@@ -82,28 +106,13 @@
 				<div class="row gx-md-8 gx-xl-12 align-items-center">
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1 mt-4">M. Marc Rompré</h4>
+							<h4 class="mb-1 mt-4">Carole Désilets</h4>
 							<p class="mb-0">Marguillier</p>
 						</div>
 					</div>
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1 mt-4">M. Raymond Lacroix</h4>
-							<p class="mb-0">Marguillier</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="row gx-md-8 gx-xl-12 align-items-center">
-					<div class="col-lg-6">
-						<div>
-							<h4 class="mb-1 mt-4">M. Raymond Martin</h4>
-							<p class="mb-0">Marguillier</p>
-						</div>
-					</div>
-					<div class="col-lg-6">
-						<div>
-							<h4 class="mb-1 mt-4">M. Pierre Favreau</h4>
+							<h4 class="mb-1 mt-4">Raymond Martin</h4>
 							<p class="mb-0">Marguillier</p>
 						</div>
 					</div>
@@ -112,7 +121,13 @@
 				<div class="row gx-md-8 gx-xl-12 align-items-center">
 					<div class="col-lg-6">
 						<div>
-							<h4 class="mb-1 mt-4">M. Claude Roy</h4>
+							<h4 class="mb-1 mt-4">Pierre Favreau</h4>
+							<p class="mb-0">Marguillier</p>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div>
+							<h4 class="mb-1 mt-4">Claude Roy</h4>
 							<p class="mb-0">Marguillier</p>
 						</div>
 					</div>
